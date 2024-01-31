7128b85be25fed782bc71303a3d27a89b5da8910
renatu <renatu_mb@yahoo.com.br>
2024-01-30 22:09:06 -0800

    Addition Http client - AXIOS, ContactService API

---
+++ src/api/ContactService.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8082/contacts'
+
+export async function saveContact(contact) {
+    return await axios.post(API_URL, contact)
+}
+
+export async function getAllContacts(page = 0, size = 10) {
+    return await axios.get(`${API_URL}?page=${page}&size=${size}`)
+}
+
+export async function getContact(id) {
+    return await axios.get(`${API_URL}/${id}`)
+}
+
+export async function updateContact(contact) {
+    return saveContact(contact)
+}
+
+export async function updatePhoto(formData) {
+    return await axios.put(`${API_URL}/photo`, formData)
+}
+
+export async function deleteContact(id) {
+    return await axios.delete(`${API_URL}/${id}`)
+}
+
+export async function getContactPhoto(fileName) {
+    return await axios.get(`${API_URL}/image/${fileName}`)
+}
+
